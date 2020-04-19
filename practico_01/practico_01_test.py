41fb7be659a79eee2e43a22b51ac321105dab288
Ezequiel Leonardo Castaño <elcg@gmx.com>
2020-04-19 11:45:48 +0200

    Add Flake to tests

--- practico_01/practico_01_test.py
+++ practico_01/practico_01_test.py
@@ -1,52 +1,69 @@
 # Cuando se finalice el TP se debe descomentar este archivo
 
+
 def test_ejercicio_01():
     import ejercicio_01
+
 
 def test_ejercicio_02():
     import ejercicio_02
 
+
 def test_ejercicio_03():
     import ejercicio_03
+
 
 def test_ejercicio_04():
     import ejercicio_04
 
+
 def test_ejercicio_05():
     import ejercicio_05
+
 
 def test_ejercicio_06():
     import ejercicio_06
 
+
 def test_ejercicio_07():
     import ejercicio_07
+
 
 def test_ejercicio_08():
     import ejercicio_08
 
+
 def test_ejercicio_09():
     import ejercicio_09
+
 
 def test_ejercicio_10():
     import ejercicio_10
 
+
 def test_ejercicio_11():
     import ejercicio_11
+
 
 def test_ejercicio_12():
     import ejercicio_12
 
+
 def test_ejercicio_13():
     import ejercicio_13
+
 
 def test_ejercicio_14():
     import ejercicio_14
 
+
 def test_ejercicio_15():
     import ejercicio_15
 
+
 def test_ejercicio_16():
     import ejercicio_16
+
 
 def test_ejercicio_17():
     import ejercicio_17
